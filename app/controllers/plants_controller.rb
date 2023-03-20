7999b2f0ba8aeaccd98ae5903050bfa7683ede0d
zaiky09 <zamilmozamil@outlook.com>
2023-03-20 17:49:26 +0300

    done

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,31 @@
+class PlantsController < ApplicationController
+
+    wrap_parameters format: []
+
+    def index
+        plants = Plant.all
+        render json: plants, status: :ok
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    def show
+        plant = Plant.find(params[:id])
+        if plant
+            render json: plant, status: :ok
+        else
+            render json: {error: "Plant not found"}, status: :not_found
+        end
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+
+
+end
