3273af65ab860673af443d18c5c0fb964d08206c
zaiky09 <zamilmozamil@outlook.com>
2023-03-20 17:57:08 +0300

    done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -13,7 +13,7 @@ class PlantsController < ApplicationController
     end
 
     def show
-        plant = Plant.find(params[:id])
+        plant = Plant.find_by(id:params[:id])
         if plant
             render json: plant, status: :ok
         else
